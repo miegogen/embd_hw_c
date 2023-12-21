dea82ca48f7747d24ecb906fce388b996dc6cfd1
Michael Egorov <usualgold@gmail.com>
2023-12-21 22:29:43 +0700

    Homework Lesson 1

---
+++ 001_home_work/hw_lesson1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+	printf("\n\n");
+	printf("Hello world!\n");
+	printf("\n\n");
+	printf("Let's\n");
+	printf("   go\n");
+	printf("     to walk\n");
+	printf("\n\n");
+	printf("   *\n");
+	printf("  ***\n");
+	printf(" *****\n");
+	printf("*******\n");
+	printf(" HH HH\n");
+	printf(" ZZZZZ\n");
+	printf("\n\n");
+	return 0;
+}
